b27aaa35f621493d0766dbb38917073195ba5d66
asgeir <asgeir.haaheim@hr-manager.net>
2017-06-27 11:00:37 +0200

    init

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HRMTS.WebhookClient.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            var postData = PostData.ReadFromDisk();
+
+            var output = string.Empty;
+
+            var list = postData.Items;
+
+            list.Reverse();
+
+            ViewBag.Output = list;
+
+            return View();
+        }
+
+        [HttpPost]
+        public HttpStatusCode Post()
+        {
+            var data = string.Empty;
+
+            var memoryStream = new MemoryStream();
+
+            Request.Body.CopyTo(memoryStream);
+
+            data = Encoding.UTF8.GetString(memoryStream.ToArray());
+
+            if (!string.IsNullOrWhiteSpace(data))
+            {
+                var postData = PostData.ReadFromDisk();
+
+                var headers = new List<KeyValue>();
+
+                foreach (var header in Request.Headers.ToList())
+                    headers.Add(new KeyValue(header.Key, header.Value));
+
+                postData.Items.Add(new PostItem {Body = data, Headers = headers});
+
+                PostData.WriteToDisk(postData);
+            }
+
+            return HttpStatusCode.OK;
+        }
+    }
+}
---
+++ PostData.cs
@@ -0,0 +1,115 @@
+﻿using Microsoft.AspNetCore.Hosting.Internal;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Primitives;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Xml.Serialization;
+
+namespace HRMTS.WebhookClient
+{
+    [Serializable]
+    public class PostData
+    {
+        private static string PostDataFile
+        {
+            get
+            {
+                var path = System.AppDomain.CurrentDomain.BaseDirectory + @"\PostData.xml";
+
+                return path;
+            }
+        }
+
+        public List<PostItem> Items { get; set; }
+
+        public PostData()
+        {
+            Items = new List<PostItem>();
+        }
+
+        public string ToXml()
+        {
+            try
+            {
+                var xmlSerializer = new XmlSerializer(this.GetType());
+
+                using (var textWriter = new StringWriter())
+                {
+                    xmlSerializer.Serialize(textWriter, this);
+
+                    return textWriter.ToString();
+                }
+            }
+            catch (Exception ex)
+            {
+                return string.Empty;
+            }
+        }
+
+        public static PostData FromXml(string xml)
+        {
+            try
+            {
+                var xmlSerializer = new XmlSerializer(typeof(PostData));
+
+                var textReader = new StringReader(xml);
+
+                return (PostData)xmlSerializer.Deserialize(textReader);
+            }
+            catch
+            {
+                return new PostData();
+            }
+        }
+
+        public static void WriteToDisk(PostData postData)
+        {
+            File.WriteAllText(PostDataFile, postData.ToXml());
+        }
+
+        public static PostData ReadFromDisk()
+        {
+            return !File.Exists(PostDataFile)
+                ? new PostData()
+                : PostData.FromXml(File.ReadAllText(PostDataFile));
+        }
+    }
+
+    [Serializable]
+    public class PostItem
+    {
+        public PostItem()
+        {
+            Headers = new List<KeyValue>();
+
+            Body = string.Empty;
+        }
+
+        public List<KeyValue> Headers { get; set; }
+
+        public string Body { get; set; }
+    }
+
+    [Serializable]
+    public class KeyValue
+    {
+        public KeyValue()
+        {
+        }
+
+        public KeyValue(string key, string value)
+        {
+            Key = key;
+
+            Value = value;
+        }
+
+        public string Key { get; set; }
+
+        public string Value { get; set; }
+    }
+}
